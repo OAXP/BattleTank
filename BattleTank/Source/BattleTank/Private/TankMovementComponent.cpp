67de7044a81fc79a1b50b56a3c8201843fa583a3
DESKTOP-EJV2BIU\OAXP <oaxpglitchersec@gmail.com>
2018-07-30 16:19:14 -0400

    Amélioration des touches du tank

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,13 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
 
 
+void UTankMovementComponent::Initialise(UTankTrack* L_TrackToSet, UTankTrack* R_TrackToSet)
+{
+	if (!L_TrackToSet || !R_TrackToSet) { return; }
+	L_Track = L_TrackToSet;
+	R_Track = R_TrackToSet;
+}
 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	//auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("Intend Move Forward throw: %f"), Throw);
-
+	L_Track->SetThrottle(Throw);
+	R_Track->SetThrottle(Throw);
+	//Prevent souble speed
 }
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -6,16 +6,23 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+
+class UTankTrack;
 /**
- * 
+ * Responsible from driving the tank tracks
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
 
 public:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+		void Initialise(UTankTrack* L_TrackToSet, UTankTrack* R_TrackToSet);
 	UFUNCTION(BlueprintCallable, Category = Input)
 		void IntendMoveForward(float Throw);
 	
+private:
+	UTankTrack * L_Track = nullptr;
+	UTankTrack * R_Track = nullptr;
 };
