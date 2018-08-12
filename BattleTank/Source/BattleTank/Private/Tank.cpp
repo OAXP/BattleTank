9c3ee85b7206dbb2af37aa1bb7cf43a3221db80f
OAXP <oaxpglitchersec@gmail.com>
2018-08-11 23:42:25 -0400

    171

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -13,6 +13,7 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	UE_LOG(LogTemp, Warning, TEXT("CPP Constructor"));
 }
 
 // Called when the game starts or when spawned
@@ -20,6 +21,7 @@ void ATank::BeginPlay()
 {
 	Super::BeginPlay();
 
+	UE_LOG(LogTemp, Warning, TEXT("CPP BeginPlay"));
 }
 
 void ATank::AimAt(FVector HitLocation)
